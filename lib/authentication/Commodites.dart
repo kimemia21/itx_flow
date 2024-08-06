d19ff38c6df7dd34f8f5f1b424050786c1972f66
kimemia21 <bobbymbogo71@gmail.com>
2024-08-06 13:21:52 +0300

    Done with product selection

--- lib/authentication/Commodites.dart
+++ lib/authentication/Commodites.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:itx/authentication/Documents.dart';
 import 'package:itx/authentication/Verification.dart';
 import 'package:itx/global/globals.dart';
 
@@ -11,34 +12,50 @@ class Commodites extends StatefulWidget {
 }
 
 class _CommoditesState extends State<Commodites> {
- final List<Map<String, String>> commodities = [
-    {'name': 'Copper', 'unit': '1,000 lbs'},
-    {'name': 'Wheat', 'unit': '5,000 bu'},
-    {'name': 'Gold', 'unit': '100 oz'},
-    {'name': 'Crude Oil', 'unit': '1,000 bbl'},
-    {'name': 'Soybeans', 'unit': '5,000 bu'},
+  final List<Map<String, dynamic>> commodities = [
+    {'name': 'Copper', 'unit': '1,000 lbs', 'isChecked': false},
+    {'name': 'Wheat', 'unit': '5,000 bu', 'isChecked': false},
+    {'name': 'Gold', 'unit': '100 oz', 'isChecked': false},
+    {'name': 'Crude Oil', 'unit': '1,000 bbl', 'isChecked': false},
+    {'name': 'Soybeans', 'unit': '5,000 bu', 'isChecked': false},
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: ()=>Globals.switchScreens(context: context,screen: Verification()), icon:Icon(Icons.arrow_back)),
+        leading: IconButton(
+            onPressed: () {
+              Globals.switchScreens(context: context, screen: Verification());
+            },
+            icon: Icon(Icons.arrow_back)),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+
           children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              alignment: Alignment.center,
+              child: Text("Add Commodities of interest",style: GoogleFonts.poppins(fontWeight: FontWeight.w600),),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8.0),
               child: TextField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                
+                    borderRadius: BorderRadius.circular(10)
+                  ),
                   labelText: 'Search commodities',
                   prefixIcon: Icon(Icons.search),
                   fillColor: Colors.grey[200],
                   filled: true,
                 ),
+                onChanged: (text) {
+                  // You can add search functionality here if needed
+                },
               ),
             ),
             Expanded(
@@ -47,8 +64,12 @@ class _CommoditesState extends State<Commodites> {
                 itemBuilder: (context, index) {
                   return ListTile(
                     trailing: Checkbox(
-                      value: false,
-                      onChanged: (bool? value) {},
+                      value: commodities[index]['isChecked'] ?? false,
+                      onChanged: (bool? value) {
+                        setState(() {
+                          commodities[index]['isChecked'] = value!;
+                        });
+                      },
                     ),
                     title: Text(commodities[index]['name']!),
                     subtitle: Text(commodities[index]['unit']!),
@@ -56,7 +77,30 @@ class _CommoditesState extends State<Commodites> {
                 },
               ),
             ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: GestureDetector(
+                onTap:()=>Globals.switchScreens(context: context,screen: DocumentsVericiation()),
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      color: Colors.green.shade800,
+                      borderRadius: BorderRadiusDirectional.circular(10)),
+                  child: Text(
+                    "Done",
+                    style: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w600,
+                        color: Colors.white,
+                        fontSize: 20),
+                  ),
+                ),
+              ), 
+            )
+
           ],
+
         ),
       ),
     );
---
+++ lib/authentication/Documents.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DocumentsVericiation extends StatefulWidget {
+  const DocumentsVericiation({super.key});
+
+  @override
+  State<DocumentsVericiation> createState() => _DocumentsVericiationState();
+}
+
+class _DocumentsVericiationState extends State<DocumentsVericiation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
